cbbb10d2b701cf080c8c80a03603491761d61692
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 17:56:02 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
-terraform {
+/*terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
+}*/
 module "sandbox" {
     source = "./modules"
     bucket_name="prowizt-bucket"

4365a8290e0f8df076ea8055273960c705c89878
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 17:32:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   secret_key = "Kg33Uz29/4Em0Sz5oDzRGo4ouDaZBie8wvimEGhz"
 }
 resource "aws_s3_bucket" "mybucket" {
-  count=2
+  count=2   // count to deploy same multiple resourses 
   bucket  = "${var.bucket_name}-${count.index}"
   tags    = {
   Name           = "MyS3testBucket"

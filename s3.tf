3627e44c61dacdd86fb1d317bea0d248d4b54a77
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 17:59:36 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,16 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}*/
+}
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIAQFXVA6BEFZSXKNUB"
+  secret_key = "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"
+}
 module "sandbox" {
     source = "./modules"
     bucket_name="prowizt-bucket"

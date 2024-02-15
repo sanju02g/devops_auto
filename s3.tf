3171544988c5e56f30474bc0116416367620d213
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 18:00:59 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
 }
-provider "aws" {
-  region     = "us-west-1"
-  access_key = "AKIAQFXVA6BEFZSXKNUB"
-  secret_key = "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"
-}
+
 module "sandbox" {
     source = "./modules"
     bucket_name="prowizt-bucket"

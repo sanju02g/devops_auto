345c96aa5d49d103aae0fc1dd5150167d2b0a45b
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 14:48:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "aws" {
   access_key = "AKIAQFXVA6BEFZSXKNUB"
   secret_key = "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"
 }
-/*resource "aws_s3_bucket" "mybucket" {
-  bucket  = "my-unique-bucketname"
+resource "aws_s3_bucket" "mybucket" {
+  bucket  = "${var.bucket_name}"
   tags    = {
   Name           = "MyS3testBucket"
   Environment    = "Production"
   }
-}*/
+}

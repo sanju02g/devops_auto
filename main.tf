ffc3c435e0dd1517c6f1950fc7647b0d072edfbd
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-02-15 17:27:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "aws" {
   secret_key = "Kg33Uz29/4Em0Sz5oDzRGo4ouDaZBie8wvimEGhz"
 }
 resource "aws_s3_bucket" "mybucket" {
-  bucket  = "${var.bucket_name}"
+  count=2
+  bucket  = "${var.bucket_name}-${count.index}"
   tags    = {
   Name           = "MyS3testBucket"
   Environment    = "Production"

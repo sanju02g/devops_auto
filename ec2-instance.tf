6d2cbe2cfb2e01840737706440abea8b2bb8dd36
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-04-23 15:31:32 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,1 +1,14 @@
-
+terraform{
+  required_provider{
+    aws={
+      source= "hashicorp/aws"
+      version= "~> 5.0"
+    }
+  }
+}
+provider "aws"{
+  region= "us-east-1"
+}
+module "ec2_instance"{
+  source= "./modules"
+}

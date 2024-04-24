c926facc377ffaefc8691943f7d6654631b44515
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-04-24 14:33:41 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -27,7 +27,7 @@ connection {
     user        = "ec2-user"
     private_key = file("${path.module}/minikey.pem")
     host        = self.public_ip
-    agent       = true
+    agent       = false
   }
 provisioner "remote-exec" {
     inline = [

9b479697caeb2d28b43373271374bc43af97773e
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-05-23 20:12:43 +0530

    Update main.tf

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
 
+    region = "us-east-1"
+}
+
+resource "aws_s3" "my_bucket" {
+    bucket= "my-unique-bucket"
+    tags = {
+        name = "my_bucket"
+        Envinronment = "testing"
+    }
+
+}

05b2f1dedc7772cce641c5c772847942a81c81e4
sanju02g <70808807+sanju02g@users.noreply.github.com>
2024-05-28 16:08:35 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 import subprocess
 def main():
-  subprocess.run(['python','version'])
+  subprocess.run(["python","--version"])
   commit_id = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
   print(f"the commit id is -{commit_id}")
 
